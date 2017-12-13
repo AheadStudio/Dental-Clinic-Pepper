c4b674c3679f95a15de4167cfc4fd3ebe091decc
VictorPullzz <GORPONFFFFFF@yandex.ru>
2017-12-14 01:51:04 +0400

    Slider on main

--- dev/js/dentalclinic.js
+++ dev/js/dentalclinic.js
@@ -25,9 +25,44 @@
 					}, 350);
 				});
 			},
+
+			slickSlider: {
+				init: function() {
+					var self = this;
+
+					self.mainSlider();
+				},
+
+				mainSlider: function() {
+					var slider = $(".slick-slider");
+
+					slider.slick({
+						arrows: true,
+						appendArrows: $(".slick-arrow-container"),
+						prevArrow: '<div class="slick-arrow-prev"></div>',
+						nextArrow: '<div class="slick-arrow-next"></div>',
+						infinite: true,
+						speed: 600,
+						slidesToShow: 1,
+						//autoplay: true,
+						autoplaySpeed: 6000
+					});
+
+					slider.on("swipe", function(event, slick, direction) {
+						var arrows = $(".slick-arrow-container", ".main-slider");
+
+						arrows.addClass("hide");
+						setTimeout(function() {
+							arrows.removeClass("hide");
+						}, 600);
+
+					});
+				}
+			},
 		};
 
 	})();
 
 	DENTALCLINIC.menu();
+	DENTALCLINIC.slickSlider.init();
 })(jQuery);
