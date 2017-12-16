815d28442e677d27ac6c4dcb5ca1b4124dc61d56
VictorPullzz <GORPONFFFFFF@yandex.ru>
2017-12-17 00:56:38 +0400

    Circle slider

--- dev/js/dentalclinic.js
+++ dev/js/dentalclinic.js
@@ -26,20 +26,44 @@
 				});
 			},
 
-			slickSlider: {
+			slickSliders: {
 				init: function() {
-					var self = this;
+					var self = this,
+						$slider = $(".slick-slider");
 
-					self.mainSlider();
+					self.mainSlider($slider);
+					self.servicesSlider($slider);
 				},
 
-				mainSlider: function() {
-					var $slider = $(".slick-slider"),
+				mainSlider: function(slider) {
+					var self = this,
+						$mainSlider = $(".main-slider"),
+					 	$slider = $mainSlider.find(slider),
 						$itemSlider = $(".main-slider-item", $slider);
 
 					$slider.slick({
 						arrows: true,
-						appendArrows: $(".slick-arrow-container"),
+						appendArrows: $(".slick-arrow-container", $mainSlider),
+						prevArrow: '<div class="slick-arrow-prev"></div>',
+						nextArrow: '<div class="slick-arrow-next"></div>',
+						infinite: true,
+						speed: 600,
+						slidesToShow: 1,
+						//autoplay: true,
+						autoplaySpeed: 6000
+					});
+					self.sliderEfect($slider, $mainSlider, $itemSlider);
+				},
+
+				servicesSlider: function(slider) {
+					var self = this,
+						$servicesSlider = $(".services-slider"),
+					 	$slider = $servicesSlider.find(slider),
+						$itemSlider = $(".services-slider-item", $slider);
+
+					$slider.slick({
+						arrows: true,
+						appendArrows: $(".slick-arrow-container", $servicesSlider),
 						prevArrow: '<div class="slick-arrow-prev"></div>',
 						nextArrow: '<div class="slick-arrow-next"></div>',
 						infinite: true,
@@ -49,32 +73,84 @@
 						autoplaySpeed: 6000
 					});
 
-					$slider.on("swipe", function(event, slick, direction) {
-						var $arrows = $(".slick-arrow-container", ".main-slider");
-
-						$arrows.addClass("hide");
-						setTimeout(function() {
-							$arrows.removeClass("hide");
-						}, 600);
-
+					$slider.on("swipe", function(event, slick, direction){
+						if (direction == "left") {
+							self.servicesCalculation("nextElement")
+						} else {
+							self.servicesCalculation("prevElement")
+						}
 					});
 
-					$itemSlider.on("mousedown", function() {
+					self.sliderEfect(false, $servicesSlider, $itemSlider);
+
+					self.servicesCalculation();
+				},
+
+				sliderEfect: function(slider, container, itemSlider) {
+					if (slider != false) {
+						slider.on("swipe", function(event, slick, direction) {
+							var $arrows = $(".slick-arrow-container", container);
+
+							$arrows.addClass("hide");
+							setTimeout(function() {
+								$arrows.removeClass("hide");
+							}, 600);
+
+						});
+					}
+
+					itemSlider.on("mousedown", function() {
 						item = $(this);
 						item.css("cursor", "-webkit-grab");
 					})
 
-					$itemSlider.on("mouseup", function() {
+					itemSlider.on("mouseup", function() {
 						item = $(this);
 						item.css("cursor", "pointer");
 					})
 
-				}
+				},
+
+				servicesCalculation: function(number) {
+					var self = this,
+						$containerCircle = $(".services-container"),
+						degContainer = $containerCircle.attr("data-deg");
+						$items = $(".services-item", $containerCircle),
+						itemsQuantity = $items.length,
+						radius = 360 / itemsQuantity;
+						wContainerCircle = $containerCircle.outerWidth(),
+						hContainerCircle = $containerCircle.outerHeight();
+
+					if (number == "nextElement") {
+						var number = Number(degContainer) - radius;
+					} else {
+						var number = Number(degContainer) + radius;
+					}
+					$containerCircle.attr("data-deg", number);
+					$items.each(function(index, element) {
+						el = $(this);
+						xCoor = (wContainerCircle / 2 * Math.cos( (number * Math.PI) / 180 )) + ((wContainerCircle / 2) - 50);
+						yCoor = (wContainerCircle / 2 * Math.sin( (number * Math.PI) / 180 )) + ((wContainerCircle / 2) - 50);
+
+						el.css({
+							"left" : Math.round(xCoor),
+							"top"  : Math.round(yCoor),
+						});
+
+						el.removeClass("active");
+						if (Math.round(xCoor) == -50 && Math.round(yCoor) == 300) {
+							el.addClass("active");
+						}
+						number = number + radius;
+					});
+
+				},
+
 			},
 		};
 
 	})();
 
 	DENTALCLINIC.menu();
-	DENTALCLINIC.slickSlider.init();
+	DENTALCLINIC.slickSliders.init();
 })(jQuery);
